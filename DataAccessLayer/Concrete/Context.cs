e43a29f37940f4920324d6a4a8e9a6f0e6639744
Mesut Kızılay <mesutkizilay3868@gmail.com>
2023-07-19 01:58:24 +0300

    N-tier architecture was build and entites were added.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+	public class Context:IdentityDbContext<AppUser,AppRole,int>
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("server=DESKTOP-8DD9NIJ;initial catalog=EasyCashDb;integrated security=true");
+		}
+
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/CustomerAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+	public class CustomerAccount
+	{
+        public int Id { get; set; }
+		public string CustomerAccountNumber { get; set; }
+        public string Currency { get; set; }
+        public decimal AccountBalance { get; set; }
+        public string BankBranch { get; set; }
+    }
+}
