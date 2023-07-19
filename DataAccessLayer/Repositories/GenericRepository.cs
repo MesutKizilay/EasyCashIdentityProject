c8e9be50a97666aaf92ed58df3c1390328fca2fe
Mesut Kızılay <mesutkizilay3868@gmail.com>
2023-07-20 02:44:40 +0300

    Dtos and FluentValidation were added.

---
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+	public class GenericRepository<T> : IGenericDal<T> where T : class
+	{
+		public void Add(T entity)
+		{
+			using var context = new Context();
+			context.Set<T>().Add(entity);
+			context.SaveChanges();
+		}
+
+		public void Delete(T entity)
+		{
+			using var context = new Context();
+			context.Set<T>().Remove(entity);
+			context.SaveChanges();
+		}
+
+		public T GetById(int id)
+		{
+			using var context =new Context();
+			return context.Set<T>().Find(id);
+		}
+
+		public List<T> GetList()
+		{
+			using var context = new Context();
+			return context.Set<T>().ToList();
+		}
+
+		public void Update(T entity)
+		{
+			using var context = new Context();
+			context.Set<T>().Update(entity);
+			context.SaveChanges();
+		}
+	}
+}
--- EntityLayer/Concrete/CustomerAccount.cs
+++ EntityLayer/Concrete/CustomerAccount.cs
@@ -13,5 +13,7 @@ namespace EntityLayer.Concrete
         public string Currency { get; set; }
         public decimal AccountBalance { get; set; }
         public string BankBranch { get; set; }
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
     }
 }
